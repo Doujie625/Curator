43fcb41405ddbe5a384ff816cf2dfe8d7fe5ce1b
Devin Jansa <devinjansa@Devins-MacBook-Pro-2.local>
2022-06-02 11:22:01 -0700

    fixed nested fetch calls

--- src/Components/ArtTile.js
+++ src/Components/ArtTile.js
@@ -1,18 +1,18 @@
 const ArtTile = (props) => {
-  if (props.art.image_id) {
+  if (props.art.data.image_id) {
     return (
-      <div key={props.art.id} className="component_element">
+      <div key={props.art.data.id} className="component_element">
         <div className="search_item">
           <div className="image">
             <img
-              src={`https://www.artic.edu/iiif/2/${props.art.image_id}/full/400,/0/default.jpg`}
+              src={`https://www.artic.edu/iiif/2/${props.art.data.image_id}/full/400,/0/default.jpg`}
               alt=""
             />
           </div>
-          <div className="plaque" onClick={() => props.addToGallery(props.art)}>
-            <h4>{props.art.artist}</h4>
-            <h4>{props.art.origin}</h4>
-            <h4>{props.art.art_type}</h4>
+          <div className="plaque" onClick={() => props.addToGallery(props.art.data)}>
+            <h4>{props.art.data.artist_title}</h4>
+            <h4>{props.art.data.place_of_origin}</h4>
+            <h4>{props.art.data.artwork_type_title}</h4>
             <a onClick={() => props.addToGallery(props.art)}>Add to Gallery</a>
           </div>
         </div>
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,87 +1,71 @@
 import React, { useEffect, useState } from "react";
-import ArtTile from "./ArtTile"
+import ArtTile from "./ArtTile";
 
 function Form(props) {
-    const [searchTerm, setSearchTerm] = useState("") //What is searched for
-    const [selection, setSelection] = useState({})
-    const [secondFetch, setSecondFetch] = useState([]) // An object made from the second fetch call with important details for each object
-    
-    useEffect(() => {
-        console.log('render')
-    }, [searchTerm])
+  const [searchTerm, setSearchTerm] = useState(""); //What is searched for
+  const [firstFetch, setFirstFetch] = useState([]);
+  const [secondFetch, setSecondFetch] = useState([]); // An object made from the second fetch call with important details for each object
 
-    const updateSearchTerm = (event) => {
-        setSearchTerm(event.target.value)
-    }
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        setSearchTerm("")
+  const updateSearchTerm = (event) => {
+    setSearchTerm(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSearchTerm("");
+    firstCall(searchTerm);
+  };
 
-        API(searchTerm).then(data => {
-            const copy = [];
-            const copyTwo = []
-            for (let i = 0; i < 10; i++){
-                const obj = {
-                    id: data.data[i].id,
-                    title: data.data[i].title,
-                }
-                copy.push(obj);
-                let term = data.data[i].id;
-                SecondCall(term).then(info => {
-                    const objTwo = {
-                        id: info.data.id,
-                        artist: info.data.artist_title,
-                        artist_date: "",
-                        origin: info.data.place_of_origin,
-                        art_type: info.data.artwork_type_title,
-                        image_id: info.data.image_id,
-                        date_display: info.data.date_display,
-                        desc: info.data.medium_display,
-                        credit: info.data.credit_line,
-                    }
-                    copyTwo.push(objTwo)
-                    setSecondFetch(copyTwo)
-                })
-            }
-        })
-        setSearchTerm("")
+  const firstCall = (url) => {
+    const baseUrl = "https://api.artic.edu/api/v1/artworks/search?q=";
+    fetch(baseUrl + url)
+      .then((res) => res.json())
+      .then((data) => {
+        let copy = [];
+        for (let i = 0; i < 10; i++) {
+          copy.push(data.data[i].id);
+        }
+        console.log(copy);
+        secondCall(copy);
+      });
 
-    }
-    async function API(url){
-        setSearchTerm("")
-        const baseUrl = 'https://api.artic.edu/api/v1/artworks/search?q=';
-        const response = await fetch(baseUrl + url);
-        const data = await response.json();
-        return data;
-    }
-    async function SecondCall(id){
-        const secondUrl = 'https://api.artic.edu/api/v1/artworks/'
-        const response = await fetch(secondUrl + id);
-        const info = await response.json();
-        return info
-    }
-    const addToGallery = (e) => {
-        const tempArray = [...props.gallery]
-        tempArray.push(e)
-        props.setGallery(tempArray)
-    }
+  };
 
-    const secondDisplay = secondFetch.map((item) => {
-        return <ArtTile art={item} addToGallery={addToGallery}/>
+  const secondCall = (results) => {
+    let secondCopy = [...firstFetch];
+    Promise.all(results.map(entry => {
+        return fetch(`https://api.artic.edu/api/v1/artworks/` + entry)
+          .then((res) => res.json())
+          .then((data) => {
+            secondCopy.push(data);
+          });
+      })
+    ).then(() => {
+        setSecondFetch(secondCopy)
+        console.log(secondCopy)
     })
+  };
 
-    return (
-        <div className="Form">
-            <main>
-            <h3>Form</h3>
-            <form onSubmit={handleSubmit}>
-                <input onChange={updateSearchTerm} value={searchTerm} type="text" placeholer="Search" />
-                <input type="submit" value="Find Art"/>
-            </form>
-            {secondDisplay}
-            </main>
-        </div>
-    )
+  const secondDisplay = secondFetch.map((item) => {
+    return <ArtTile art={item} />
+})
+
+  return (
+    <div className="Form">
+      <main>
+        <h3>Form</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            onChange={updateSearchTerm}
+            value={searchTerm}
+            type="text"
+            placeholer="Search"
+          />
+          <input type="submit" value="Find Art" />
+        </form>
+        {secondDisplay}
+      </main>
+    </div>
+  );
 }
 
 export default Form;
